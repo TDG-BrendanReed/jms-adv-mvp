a93e39c90ffc8d8097bfc3ea4365c3f034c952fb
Brendan Reed <brendan.james.reed@gmail.com>
2021-10-02 14:02:05 +1000

    Testing DB Connection

--- src/App.js
+++ src/App.js
@@ -1,18 +1,78 @@
 import React, { useState, useEffect } from 'react';
 
+import './App.css';
+
+
+
 function App() {
-  const [data, setData] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState(null)
+
+  async function loadTasks() {
+    try {
+        // Uses fetch to call server
+        const response = await fetch('/api/tasks');
+        // Reads returned JSON, which contains one property called tasks
+        const retrievedData = await response.json();
+        // Retrieve tasks, which contains an array of all tasks in database
+        const retrievedTasks = retrievedData.tasks;
+        // Loop through all tasks
+        for (let task of retrievedTasks) {
+            // Add each task to the array
+            setTasks(tasks.push(task));
+            console.log(tasks);
+        }    
+    } catch {
+        // If there is an error, display a generic message on the page
+       console.log('an error occured')
+    }
+  }
+
+  async function postTask() {
+    const response = await fetch(
+      '/api/tasks', // API location
+      {
+          method: 'POST', // POST to create new item
+          body: JSON.stringify(taskInput), // Add task to body
+          headers: {
+              'Content-Type': 'application/json' // Set return type to JSON
+          }
+      }
+  );
+  console.log('Response: ')
+  console.log(response)
+    }
+
+  const handleChange = (e) => {
+    setTaskInput(e.target.value);
+    console.log(taskInput)
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(taskInput);
+    // ... submit to API or something
+    // Call server
+    postTask()
+    
+  };
 
   useEffect(() => {
-    (
-      async function () { 
-        const { text } = await( await fetch(`/api/message`)).json();
-      setData(text);
-      }) ();
+    loadTasks()    
     
   });
 
-  return <div>{data}</div>;
+  return <article>
+  <h1>My tasks</h1>
+  <ul id="task-list"></ul>
+
+  <div>
+      <label for="name">Title: </label><input type="text" name="taskInput" onChange={handleChange} />
+  </div>
+  <div>
+      <button type="button" onClick={handleSubmit}>Add task</button>
+  </div>
+</article>;
 }
 
 export default App;
