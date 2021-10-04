03283b414bb329becddd404c723c08875926ebd6
Brendan Reed <brendan.james.reed@gmail.com>
2021-10-04 19:08:15 +1100

    fix eslint issue

--- src/components/ProfileContent.js
+++ src/components/ProfileContent.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import { Spinner } from "react-bootstrap";
 import { useMsal } from "@azure/msal-react";
