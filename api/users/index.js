a7b76856ab78382866c5d8720e6abd6f4ccc28b9
Brendan Reed <brendan.james.reed@gmail.com>
2021-10-04 18:21:55 +1100

    updated api to pull all the data needed

--- api/users/index.js
+++ api/users/index.js
@@ -1,76 +1,83 @@
 // Load mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // Connect to the database
 mongoose.connect(
-    
-    process.env.CONNECTION_STRING, // Retrieve connection string
-    { // boiler plate values
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
+  process.env.CONNECTION_STRING, // Retrieve connection string
+  {
+    // boiler plate values
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
 // Create the schema or structure of our object in Mongoose
 const userSchema = new mongoose.Schema({
-    userId: String, 
-    userData: { 
-        firstName: String, 
-        surname: String,
-        email: String 
-    }
+  userId: String,
+  userData: {
+    businessPhones: Array,
+    givenName: String,
+    surname: String,
+    userPrincipalName: String,
+    displayName: String,
+    id: String,
+    jobTitle: String,
+    mail: String,
+    mobilePhone: String,
+    officeLocation: String,
+  },
 });
 
 // Create a model using our schema
 // This model will be used to access the database
-const UserModel = mongoose.model('user', userSchema);
-console.log(UserModel)
+const UserModel = mongoose.model("user", userSchema);
+console.log(UserModel);
 
 // Export our function
 module.exports = async function (context, req) {
-    // setup our default content type (we always return JSON)
-    context.res = {
-        header: {
-            "Content-Type": "application/json"
-        }
-    }
+  // setup our default content type (we always return JSON)
+  context.res = {
+    header: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    // Read the method and determine the requested action
-    switch (req.method) {
-        // If get, return all tasks
-        case 'GET':
-            await getUser(context);
-            break;
-        // If post, create new task
-        case 'POST':
-            await createUser(context);
-            break;
-       /* // If put, update task
+  // Read the method and determine the requested action
+  switch (req.method) {
+    // If get, return all tasks
+    case "GET":
+      await getUser(context);
+      break;
+    // If post, create new task
+    case "POST":
+      await createUser(context);
+      break;
+    /* // If put, update task
         case 'PUT':
             await updateTask(context);
             break;
     */
-        }
+  }
 };
 
 // Return user with supplied UID
 async function getUser(context) {
-    console.log(context)
-    // load user found from database
-    const user = await UserModel.find({userId: context.bindingData.id});
-    // return user
-    context.res.body = { user: user };
+  console.log(context);
+  // load user found from database
+  const user = await UserModel.find({ userId: context.bindingData.id });
+  // return user
+  context.res.body = { user: user };
 }
 
 // Create new User
 async function createUser(context) {
-    // Read the uploaded user
-    const body = context.req.body;
-    // Save to database
-    const user = await UserModel.create(body);
-    // Set the HTTP status to created
-    context.res.status = 201;
-    // return new object
-    context.res.body = user;
+  // Read the uploaded user
+  const body = context.req.body;
+  // Save to database
+  const user = await UserModel.create(body);
+  // Set the HTTP status to created
+  context.res.status = 201;
+  // return new object
+  context.res.body = user;
 }
 /*
 // Update an existing function
@@ -91,4 +98,3 @@ async function updateTask(context) {
     }
 }
     */
-
