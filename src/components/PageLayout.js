fd327b8ad22b55e9602b7d1c0960b1b940d80c25
Brendan Reed <brendan.james.reed@gmail.com>
2021-10-04 17:02:08 +1100

    test adding to database

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -13,10 +13,10 @@ export const PageLayout = (props) => {
     return (
         <>
             <Navbar bg="primary" variant="dark">
-                <a className="navbar-brand" href="/">MSAL React Tutorial</a>
+                <a className="navbar-brand" href="/">MVP</a>
                 { isAuthenticated ? <SignOutButton /> : <SignInButton /> }
             </Navbar>
-            <h5><center>Welcome to the Microsoft Authentication Library For React Tutorial</center></h5>
+            <h5><center>Welcome</center></h5>
             <br />
             <br />
             {props.children}
--- src/components/ProfileContent.js
+++ src/components/ProfileContent.js
@@ -10,7 +10,10 @@ function ProfileContent() {
     const [graphData, setGraphData] = useState(null);
     const [user, setUser] = useState({
         MSALData: null,
-        userId: null
+        userId: null,
+        firstName: null,
+        surname: null,
+        email: null
     })
     const name = accounts[0] && accounts[0].name;
     const tempId = accounts[0] && accounts[0].localAccountId;
@@ -31,7 +34,14 @@ function ProfileContent() {
           const retrievedData = await response.json();
           // Retrieve tasks, which contains an array of all tasks in database
           console.log("retrieve user")
-          console.log(retrievedData)
+          console.log(retrievedData.user)
+          if(retrievedData.user.length < 1){
+            console.log("requesting profile data flow")  
+            RequestProfileData()
+          } else {
+              console.log("array length less than 1")
+          }
+
           
         
       } catch (error) {
@@ -43,6 +53,24 @@ function ProfileContent() {
     
     }
 
+    async function postUser() {
+        console.log("made it into function")
+        const response = await fetch(
+          '/api/users', // API location
+          {
+              method: 'POST', // POST to create new item
+              body: JSON.stringify(user), // Add task to body
+              headers: {
+                  'Content-Type': 'application/json' // Set return type to JSON
+              }
+              
+          }
+          
+      );
+      console.log('Response: ')
+      console.log(response)
+        }
+
     function RequestProfileData() {
         const request = {
             ...loginRequest,
@@ -52,12 +80,21 @@ function ProfileContent() {
         // Silently acquires an access token which is then attached to a request for Microsoft Graph data
         instance.acquireTokenSilent(request).then((response) => {
             callMsGraph(response.accessToken).then(response => setGraphData(response));
+            setUser({
+                firstName: graphData.givenName,
+                surname: graphData.surname,
+                email: graphData.userPrincipalName
+            })
+            console.log("posting user")
+            console.log(user)
+            postUser()
             
             
         }).catch((e) => {
             instance.acquireTokenPopup(request).then((response) => {
                 callMsGraph(response.accessToken).then(response => setGraphData(response));
-                
+                console.log("posting user from catch")
+                postUser()
                 
             });
         });
