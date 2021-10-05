bd3ccd0a5638119577f97f6bec63b2ddbd767a12
Brendan Reed <brendan.james.reed@gmail.com>
2021-10-05 13:33:09 +1100

    test changed variable names

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,31 +1,37 @@
 import React from "react";
-import { Table } from "react-bootstrap";
+import { Container, Table } from "react-bootstrap";
 
 function UserList(props) {
   console.log(props);
   console.log(props.userList);
   return (
     <>
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>ID#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.userList.user.map((userItem, i) => (
-            <tr id={i}>
-              <td>{userItem._id}</td>
-              <td>{userItem.givenName}</td>
-              <td>{userItem.surname}</td>
-              <td>{userItem.userPrincipalName}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+      <Container>
+        <Row>
+          <Col>
+            <Table striped bordered hover size="sm">
+              <thead>
+                <tr>
+                  <th>ID#</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+              <tbody>
+                {props.userList.user.map((userItem, i) => (
+                  <tr id={i}>
+                    <td>{userItem._id}</td>
+                    <td>{userItem.userData.givenName}</td>
+                    <td>{userItem.userData.surname}</td>
+                    <td>{userItem.userData.userPrincipalName}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
